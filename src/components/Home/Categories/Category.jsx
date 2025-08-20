3f49fc0f061000d2979b0e8c6ea73571f4b7d08a
unknown <sakibahamedkhan01@mgmail.com>
2025-08-20 16:04:21 +0600

    first commit

---
+++ src/components/Home/Categories/Category.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import apiClient from "../../../services/api-client";
+import CategoryItems from "./CategoryItems";
+
+const Category = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    apiClient.get("/categories/").then((res) => setCategories(res.data));
+  }, []);
+  return (
+    <section className="py-12 px-4 max-w-7xl mx-auto">
+      {/* Category Heading  */}
+      <div className="flex justify-between items-center mb-8">
+        <h2 className="text-3xl font-bold">Browse Categories</h2>
+      </div>
+
+      {/* Category Grid  */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {categories.map((category, index) => (
+          <CategoryItems key={category.id} index={index} category={category} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Category;
---
+++ src/hooks/useCart.js
@@ -0,0 +1,111 @@
+import { useCallback, useEffect, useState } from "react";
+import authApiClient from "../services/auth-api-client";
+
+const useCart = () => {
+  const [authToken] = useState(
+    () => JSON.parse(localStorage.getItem("authTokens"))?.access
+  );
+  const [cart, setCart] = useState(null);
+  const [cartId, setCartId] = useState(() => localStorage.getItem("cartId"));
+  const [loading, setLoading] = useState(false);
+
+  // Crate a new cart
+  const createOrGetCart = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await authApiClient.post("/carts/");
+      if (!cartId) {
+        localStorage.setItem("cartId", response.data.id);
+        setCartId(response.data.id);
+      }
+      setCart(response.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [cartId]);
+
+  const getCart = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await authApiClient.get("/carts/");
+      console.log(response);
+      setCart(response.data[0]);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  // Add items to the cart
+  const AddCartItems = useCallback(
+    async (product_id, quantity) => {
+      setLoading(true);
+      if (!cartId) await createOrGetCart();
+      try {
+        const response = await authApiClient.post(`/carts/${cartId}/items/`, {
+          product_id,
+          quantity,
+        });
+        await getCart();
+        return response.data;
+      } catch (error) {
+        console.log("Error adding Items", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [cartId, createOrGetCart]
+  );
+
+  // Update Item quantity
+  const updateCartItemQuantity = useCallback(
+    async (itemId, quantity) => {
+      try {
+        await authApiClient.patch(`/carts/${cartId}/items/${itemId}/`, {
+          quantity,
+        });
+      } catch (error) {
+        console.log("Error updating cart items", error);
+      }
+    },
+    [cartId]
+  );
+
+  // Delete Cart Items
+  const deleteCartItems = useCallback(
+    async (itemId) => {
+      try {
+        await authApiClient.delete(`/carts/${cartId}/items/${itemId}/`);
+        await getCart();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [cartId]
+  );
+
+  useEffect(() => {
+    const initializeCart = async () => {
+      setLoading(true);
+      await getCart();
+      setLoading(false);
+    };
+    initializeCart();
+  }, []);
+
+  return {
+    cart,
+    loading,
+    cartId,
+    createOrGetCart,
+    AddCartItems,
+    updateCartItemQuantity,
+    deleteCartItems,
+    getCart,
+  };
+};
+
+export default useCart;
